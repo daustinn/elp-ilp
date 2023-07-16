32f56b25a889dd97a53fea5ad7102fd6bf8b39d3
Daustinn Dev <57356474+daustinn@users.noreply.github.com>
2023-07-15 19:54:05 -0500

    Database query change

--- database query.sql
+++ database query.sql
@@ -5,8 +5,6 @@ CREATE TABLE IF NOT EXISTS `departamento` (
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO `departamento` (`id`, `nombre`) VALUES (1, 'General');
-
 
 -- AREA TABLE
 CREATE TABLE IF NOT EXISTS `area` (
@@ -18,7 +16,6 @@ CREATE TABLE IF NOT EXISTS `area` (
   CONSTRAINT `iddepartamento` FOREIGN KEY (`iddepartamento`) REFERENCES `departamento` (`id`)
 );
 
-INSERT INTO `area` (`id`, `nombre`, `iddepartamento`) VALUES (1, 'Director General', 1);
 
 
 -- SEDE TABLE
@@ -154,3 +151,120 @@ CREATE TABLE IF NOT EXISTS `evaluacion` (
   CONSTRAINT `idevaluador` FOREIGN KEY (`idevaluador`) REFERENCES `colaborador` (`id`),
   CONSTRAINT `idpuntaje` FOREIGN KEY (`idpuntaje`) REFERENCES `puntaje` (`id`)
 );
+
+-- QUERY USER ADD
+INSERT INTO `rol` VALUES (1, 'admin');
+INSERT INTO `rol` VALUES (2, 'colab');
+
+INSERT INTO `usuario` VALUES (1, 'root', 'root',1);
+INSERT INTO `usuario` VALUES (2, 'colab','colab',2);
+
+INSERT INTO `departamento` VALUES (1,'General');
+INSERT INTO `departamento` VALUES (2,'Administrativa');
+INSERT INTO `departamento` VALUES (3,'Academico');
+INSERT INTO `departamento` VALUES (4,'Investigacion e Innovacion');
+INSERT INTO `departamento` VALUES (5,'Marketing y Comercial');
+INSERT INTO `departamento` VALUES (6,'Secretaria Academica');
+INSERT INTO `departamento` VALUES (7,'Educacion Continua');
+INSERT INTO `departamento` VALUES (8,'Centro de Idiomas');
+
+INSERT INTO `puesto` VALUES (1,1,'Director');
+INSERT INTO `puesto` VALUES (2,2,'Asesor');
+INSERT INTO `puesto` VALUES (3,3,'Jefe');
+INSERT INTO `puesto` VALUES (4,4,'Coordinador');
+INSERT INTO `puesto` VALUES (5,5,'Analista');
+INSERT INTO `puesto` VALUES (6,6,'Asistente');
+INSERT INTO `puesto` VALUES (7,7,'Practicante');
+INSERT INTO `puesto` VALUES (8,8,'Operario');
+
+INSERT INTO `sede` VALUES (1,'Alameda');
+INSERT INTO `sede` VALUES (2,'Jazmines');
+INSERT INTO `sede` VALUES (3,'Casuarinas');
+
+INSERT INTO `usuario` VALUES (1,'admin','admin',1);
+INSERT INTO `usuario` VALUES (2,'joaocisneros@elp.edu.pe','123',2);
+INSERT INTO `usuario` VALUES (3,'dianacurilla@elp.edu.pe','123',2);
+INSERT INTO `usuario` VALUES (4,'henrysalcedo@elp.edu.pe','123',2);
+
+INSERT INTO `cargo` VALUES (1,'Director general');
+INSERT INTO `cargo` VALUES (2,'Directora adjunta');
+INSERT INTO `cargo` VALUES (3,'Director administrativo');
+INSERT INTO `cargo` VALUES (4,'Director academico');
+INSERT INTO `cargo` VALUES (5,'Director de investigacion e innovacion');
+INSERT INTO `cargo` VALUES (6,'Asesor legal');
+INSERT INTO `cargo` VALUES (7,'Asesor de contabilidad y tributacion');
+INSERT INTO `cargo` VALUES (8,'Asesor de gestion estrategica');
+INSERT INTO `cargo` VALUES (9,'Jefe de Infraestructura');
+INSERT INTO `cargo` VALUES (10,'Jefe de Admision ELP');
+INSERT INTO `cargo` VALUES (11,'Jefe Secretaria Academica ILP y ELP');
+INSERT INTO `cargo` VALUES (12,'Jefe de Registro de Grados y Titulos');
+INSERT INTO `cargo` VALUES (13,'Jefe Academico C. Salud');
+INSERT INTO `cargo` VALUES (14,'Jefe de Comunicaciones');
+INSERT INTO `cargo` VALUES (15,'Jefe de Sistemas');
+INSERT INTO `cargo` VALUES (16,'Jefe Academico C. Gestion y Tecnologia');
+INSERT INTO `cargo` VALUES (17,'Jefe de Admision ILP');
+INSERT INTO `cargo` VALUES (18,'Jefe gestion personas');
+INSERT INTO `cargo` VALUES (19,'Coordinador Academico C. Salud');
+INSERT INTO `cargo` VALUES (20,'Coordinador Certificaciones');
+INSERT INTO `cargo` VALUES (21,'Coordinadora de Experiencia del Estudiante');
+INSERT INTO `cargo` VALUES (22,'Coordinadora Secretaria Academica ILP ');
+INSERT INTO `cargo` VALUES (23,'Coordinador de Comunicacion ILP');
+INSERT INTO `cargo` VALUES (24,'Coordinador de Contabilidad');
+INSERT INTO `cargo` VALUES (25,'Coordinador de Recaudacion');
+INSERT INTO `cargo` VALUES (26,'Coordinador de Calidad Educativa');
+INSERT INTO `cargo` VALUES (27,'Coordinador de Educacion Continua');
+INSERT INTO `cargo` VALUES (28,'Coordinadora de Tesoreria');
+INSERT INTO `cargo` VALUES (29,'Coordinador de Academico C. Gestion y TI');
+INSERT INTO `cargo` VALUES (30,'Coordinador de Servicios y Demanda');
+INSERT INTO `cargo` VALUES (31,'Coordinador de Comunicaciones: Cybernet, RRPP y Prensa');
+INSERT INTO `cargo` VALUES (32,'Jefe de Gestion de Personas');
+INSERT INTO `cargo` VALUES (33,'Coordinador de Comunicaciones ELP');
+INSERT INTO `cargo` VALUES (34,'Coordinador de cloud, redes y conectividad');
+INSERT INTO `cargo` VALUES (35,'Coordinador de Comunicaciones: Educacion Continua y Centro de Idiomas');
+INSERT INTO `cargo` VALUES (36,'Coordinador de RATEC');
+INSERT INTO `cargo` VALUES (37,'Coordinador del Centro de Idiomas');
+INSERT INTO `cargo` VALUES (38,'Coordinador del Centro de Información');
+INSERT INTO `cargo` VALUES (39,'Coordinado de Bienestar');
+INSERT INTO `cargo` VALUES (40,'Coordinador de Talento');
+INSERT INTO `cargo` VALUES (41,'Analista de Contabilidad');
+INSERT INTO `cargo` VALUES (42,'Analista de Secretaria Academica del ILP');
+INSERT INTO `cargo` VALUES (43,'Analista Legal');
+INSERT INTO `cargo` VALUES (44,'Analista de mesa de ayuda');
+INSERT INTO `cargo` VALUES (45,'Analista de Calidad Educativa');
+INSERT INTO `cargo` VALUES (46,'Analista de Diseño y Producción ILP');
+INSERT INTO `cargo` VALUES (47,'Analista de Experiencia del Estudiante');
+INSERT INTO `cargo` VALUES (48,'Asistente de Direccion General');
+INSERT INTO `cargo` VALUES (49,'Asistente de Comunicaciones: Educacion Continua y Centro de Idiomas');
+INSERT INTO `cargo` VALUES (50,'Asistente de Tesoreia');
+INSERT INTO `cargo` VALUES (51,'Asesor  de Admision ILP');
+INSERT INTO `cargo` VALUES (52,'Asesor de Admision idiomas');
+INSERT INTO `cargo` VALUES (53,'Asistente Modulos');
+INSERT INTO `cargo` VALUES (54,'Asistente Secretaria Academica ILP');
+INSERT INTO `cargo` VALUES (55,'Asistente de Certificaciones');
+INSERT INTO `cargo` VALUES (56,'Asistente Administrativo');
+INSERT INTO `cargo` VALUES (57,'Asistente de Infraestructura');
+INSERT INTO `cargo` VALUES (58,'Asesor  de Admision ILP Y ELP');
+INSERT INTO `cargo` VALUES (59,'Asistente de Bienestar');
+INSERT INTO `cargo` VALUES (60,'Asistente Academico C. Gestion y Tecnologia ');
+INSERT INTO `cargo` VALUES (61,'Asistente Secretaria Academica ELP');
+INSERT INTO `cargo` VALUES (62,'Asistente de Redes Sociales');
+INSERT INTO `cargo` VALUES (63,'Asistente de Sistemas');
+INSERT INTO `cargo` VALUES (64,'Asistente de Seguridad y Camaras');
+INSERT INTO `cargo` VALUES (65,'Asistente de Experiencia del Estudiante');
+INSERT INTO `cargo` VALUES (66,'Asistente de Topico ');
+INSERT INTO `cargo` VALUES (67,'Asistente de Registro de Grados y Titulos');
+INSERT INTO `cargo` VALUES (68,'Asistente de Contabilidad');
+INSERT INTO `cargo` VALUES (69,'Asistente de Calidad Educativa');
+INSERT INTO `cargo` VALUES (70,'Asistente Academico C. Salud');
+INSERT INTO `cargo` VALUES (71,'Asesor de Admision ELP');
+INSERT INTO `cargo` VALUES (72,'Asesor de Admision Continua');
+INSERT INTO `cargo` VALUES (73,'Asistente de Recaudacion');
+INSERT INTO `cargo` VALUES (74,'Practicante de Certificaciones');
+INSERT INTO `cargo` VALUES (75,'Practicante de mesa de ayuda');
+INSERT INTO `cargo` VALUES (76,'Practicante Secretaria Academica ILP');
+INSERT INTO `cargo` VALUES (77,'Practicante de Comunicaciones');
+INSERT INTO `cargo` VALUES (78,'Practicante de Experiencia del Estudiante');
+INSERT INTO `cargo` VALUES (79,'Mentor');
+INSERT INTO `cargo` VALUES (80,'Operario de Logistica');
+INSERT INTO `cargo` VALUES (81,'Operario de Seguridad');
+INSERT INTO `cargo` VALUES (82,'Operario de Limpieza');
