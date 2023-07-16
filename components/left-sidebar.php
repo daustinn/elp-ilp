8da9102168e83f364c9ffa13c4b0ad514134ca40
Daustinn Dev <57356474+daustinn@users.noreply.github.com>
2023-07-16 00:23:53 -0500

    hidden if user not admin type

--- components/left-sidebar.php
+++ components/left-sidebar.php
@@ -1,3 +1,9 @@
+<?php
+// Session start
+$rol = $_SESSION['rol'];
+?>
+
+
 <div class="left-side-bar">
     <div class="brand-logo">
         <a href="index.php">
@@ -25,46 +31,52 @@
                         <span class="micon bi bi-calendar4-week"></span><span class="mtext">Calendario</span>
                     </a>
                 </li>
-                <li class="dropdown">
-                    <a href="javascript:;" class="dropdown-toggle">
-                        <span class="micon bi bi-pie-chart"></span><span class="mtext">Reportes</span>
-                    </a>
-                    <ul class="submenu">
-                        <li><a href="highchart.html">Objetivos</a></li>
-                        <li><a href="knob-chart.html">Usuarios</a></li>
-                        <li><a href="jvectormap.html">Colaboradores</a></li>
-                    </ul>
-                </li>
-                <li class="">
-                    <a href="javascript:;" class="dropdown-toggle no-arrow">
-                        <span class="micon bi bi-people"></span><span class="mtext">Usuarios</span>
-                    </a>
-                </li>
-
                 <li>
-                    <a href="sitemap.html" class="dropdown-toggle no-arrow">
-                        <span class="micon bi bi-diagram-3"></span><span class="mtext">Colaboradores</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="invoice.html" class="dropdown-toggle no-arrow">
+                    <a href="notas.php" class="dropdown-toggle no-arrow">
                         <span class="micon bi bi-receipt-cutoff"></span><span class="mtext">Notas</span>
                     </a>
                 </li>
-                <li class="dropdown">
-                    <a href="javascript:;" class="dropdown-toggle">
-                        <span class="micon bi bi-hdd-stack"></span><span class="mtext">Otros</span>
-                    </a>
-                    <ul class="submenu">
-                        <li><a href="otros/usuarios.php">Usuarios</a></li>
-                        <li><a href="otros/roles.php">Roles</a></li>
-                        <li><a href="otros/usuarios.php">Sedes</a></li>
-                        <li><a href="otros/sedes.php">Areas</a></li>
-                        <li><a href="otros/departamentos.php">Departamentos</a></li>
-                        <li><a href="otros/tipo-objetivos.php">Tipo Objetivos</a></li>
-                        <li><a href="otros/correos.php">Correos</a></li>
-                    </ul>
-                </li>
+                <!-- verfify if role user is admin -->
+                <?php
+                if ($rol == 'admin') {
+                ?>
+                    <li class="dropdown">
+                        <a href="javascript:;" class="dropdown-toggle">
+                            <span class="micon bi bi-pie-chart"></span><span class="mtext">Reportes</span>
+                        </a>
+                        <ul class="submenu">
+                            <li><a href="highchart.html">Objetivos</a></li>
+                            <li><a href="knob-chart.html">Usuarios</a></li>
+                            <li><a href="jvectormap.html">Colaboradores</a></li>
+                        </ul>
+                    </li>
+                    <li class="">
+                        <a href="javascript:;" class="dropdown-toggle no-arrow">
+                            <span class="micon bi bi-people"></span><span class="mtext">Usuarios</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="sitemap.html" class="dropdown-toggle no-arrow">
+                            <span class="micon bi bi-diagram-3"></span><span class="mtext">Colaboradores</span>
+                        </a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="javascript:;" class="dropdown-toggle">
+                            <span class="micon bi bi-hdd-stack"></span><span class="mtext">Otros</span>
+                        </a>
+                        <ul class="submenu">
+                            <li><a href="otros/usuarios.php">Usuarios</a></li>
+                            <li><a href="otros/roles.php">Roles</a></li>
+                            <li><a href="otros/usuarios.php">Sedes</a></li>
+                            <li><a href="otros/sedes.php">Areas</a></li>
+                            <li><a href="otros/departamentos.php">Departamentos</a></li>
+                            <li><a href="otros/tipo-objetivos.php">Tipo Objetivos</a></li>
+                            <li><a href="otros/correos.php">Correos</a></li>
+                        </ul>
+                    </li>
+                <?php
+                }
+                ?>
             </ul>
         </div>
     </div>
