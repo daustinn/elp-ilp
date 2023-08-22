f92e5b9ab264c156373ac64a3f8b1cb92f4242cf
Daustinn Dev <57356474+daustinn@users.noreply.github.com>
2023-08-22 13:39:34 -0500

    Add getRolById method

--- services/rol.service.php
+++ services/rol.service.php
@@ -6,13 +6,29 @@ function getRoles()
 {
     $conexion = connectToDatabase();
     if ($conexion) {
-        $query = "SELECT * FROM rol";
+        $query = "SELECT * FROM rol ORDER BY created_at desc";
         $result = $conexion->query($query);
         if ($result && $result->num_rows > 0) {
             $rol = array();
             while ($row = $result->fetch_assoc()) {
                 $rol[] = $row;
             }
+            $conexion->close();
+            return $rol;
+        }
+        $conexion->close();
+    }
+    return false;
+}
+
+function getRolById($id)
+{
+    $conexion = connectToDatabase();
+    if ($conexion) {
+        $query = "SELECT * FROM rol WHERE id = '$id'"; // Cambio en la consulta SQL
+        $result = $conexion->query($query);
+        if ($result && $result->num_rows > 0) {
+            $rol = $result->fetch_assoc(); // Obtén el primer registro
             $conexion->close();
             return $rol;
         }
